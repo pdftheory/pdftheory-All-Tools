ff6041413e51af7dea54f4f89a15ef97b0ddd96a
pdftheory <myincome512@gmail.com>
2026-02-19 13:57:10 +0200

    Add ezoicId prop to AdContainer component

--- src/components/ads/AdContainer.tsx
+++ src/components/ads/AdContainer.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { AdUnit } from './AdUnit';
 
+// This interface is the "Rulebook" that was missing the ezoicId
 interface AdContainerProps {
     slotId: string;
+    ezoicId?: string;
     className?: string;
 }
 
-export const AdContainer = ({ slotId, className = '' }: AdContainerProps) => {
+export const AdContainer = ({ slotId, ezoicId, className = '' }: AdContainerProps) => {
     return (
         <div className={`container mx-auto px-4 my-12 ${className}`}>
-            <AdUnit slotId={slotId} />
+            <AdUnit slotId={slotId} ezoicId={ezoicId} />
         </div>
     );
 };
